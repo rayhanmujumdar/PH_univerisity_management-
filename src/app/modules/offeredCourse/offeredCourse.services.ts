165dc4912321850949ce44cfd18a24e9c45869d5
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2023-12-19 02:16:28 +0600

    offered course schedule time conflict fixed

--- src/app/modules/offeredCourse/offeredCourse.services.ts
+++ src/app/modules/offeredCourse/offeredCourse.services.ts
@@ -17,6 +17,9 @@ export const createOfferedCourseService = async (payload: TOfferedCorse) => {
         semesterRegistration,
         section,
         faculty,
+        days,
+        startTime,
+        endTime,
     } = payload;
     // academic department check into database
     const isAcademicDepartmentExist =
@@ -81,5 +84,31 @@ export const createOfferedCourseService = async (payload: TOfferedCorse) => {
             `offered course section ${section} is not unique`,
         );
     }
-    return OfferCourse.create(payload);
+    const newSchedule = {
+        days,
+        startTime,
+        endTime,
+    };
+    const assignedSchedule = await OfferCourse.find({
+        semesterRegistration,
+        faculty,
+        days: { $in: days },
+    });
+    assignedSchedule.forEach((schedule) => {
+        // 11.30 - 1.30 -> that is this current class time -> existing time
+        // 12.30 - 2.30 -> that is not valid class time because you have already class -> new time
+        const existingStartTime = new Date(
+            `1970-01-01T${schedule.startTime}:00`,
+        );
+        const existingEndTime = new Date(`1970-01-01T${schedule.endTime}:00`);
+        const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}:00`);
+        const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}:00`);
+        if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+            throw new AppError(
+                httpStatus.CONFLICT,
+                `This faculty is not available this time. please choose another time and days`,
+            );
+        }
+    });
+    return OfferCourse.create({ ...payload, academicSemester });
 };
